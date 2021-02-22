4f83c94bbcb1ce6496e40e4a451191cbb0654e1f
masiyematome <matomemasiye7@gmail.com>
2021-02-22 17:37:32 +0200

    Added the about page

--- main_app.js
+++ main_app.js
@@ -3,14 +3,24 @@ const navTogglerButton = document.querySelector(".navbar-toggler");
 const navBarLinks = document.querySelector(".navbar-links");
 const closeButton = document.querySelector(".close-button");
 const menu = document.querySelector(".menu");
+const links = document.querySelectorAll(".menu a");
 
-
-navTogglerButton.addEventListener("click" , ()=> {
-    navBarLinks.classList.add("showing");
-    menu.classList.add("showing");
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        navBarLinks.classList.remove("showing");
+        document.body.style.overflow = "scroll";
+    })
 })
 
-closeButton.addEventListener("click" , ()=> {
+
+navTogglerButton.addEventListener("click", () => {
+    navBarLinks.classList.add("showing");
+    menu.classList.add("showing");
+    document.body.style.overflow = "hidden";
+})
+
+closeButton.addEventListener("click", () => {
     navBarLinks.classList.remove("showing");
     menu.classList.remove("showing");
+    document.body.style.overflow = "scroll";
 })
